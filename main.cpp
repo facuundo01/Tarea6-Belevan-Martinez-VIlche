9086a711e19a9e1356f57ca5d651209a1802ed03
Lab3-PC14 <Lab3-PC14@LAB3-PC14>
2025-10-03 11:03:55 -0300

    Agregar archivos de proyecto.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int x;
+	int y;
+	cout << "Ingrese cantidad de incognitas: ";
+	cin >> x;
+	cout << "Ingrese la cantidad de ecuaciones: ";
+	cin >> y;
+
+	
+}
