cbccadd3643a7b4e14e33dc7f6485d10eae40014
Astinv <vilchecrece@gmail.com>
2025-10-17 22:41:16 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,9 +19,36 @@ int main() {
     cout << "Ingrese terminos independientes:\n";
     for (int i = 0; i < n; i++) cin >> b[i];
 
-    resolverPorGaussJordan(A, b);
+    double det = 0;
+    if (n == 3) {
+        det = determinanteSarrus(matriz);
+        cout << "Determinante (Sarrus): " << det << endl;
+    } else if (n > 3) {
+        det = determinanteLaplace(matriz);
+        cout << "Determinante (Laplace): " << det << endl;
+    } else if (n == 2) {
+        det = matriz[0][0]*matriz[1][1] - matriz[0][1]*matriz[1][0];
+        cout << "Determinante (2x2): " << det << endl;
+    }
+
+    if (det == 0) {
+        cout << "El sistema no tiene solución única.\n";
+        return 0;
+    }
+
+    vector<double> soluciones = resolverPorGaussJordan(A, b);
+
+    cout << "\nSoluciones:\n";
+    for (int i = 0; i < soluciones.size(); i++) {
+        cout << "x" << i+1 << " = " << soluciones[i] << endl;
+    }
+
+
+    guardarResultados(soluciones, "resultados.txt");
+
 
     return 0;
 }
+
 
 
