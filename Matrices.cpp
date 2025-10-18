55f7d9cfdb4342cff6c20d6d4b7f5e4993e71abe
Astinv <vilchecrece@gmail.com>
2025-10-17 22:52:14 -0300

    Update Matrices.cpp

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,7 +1,7 @@
 #include "Matrices.h"
 
 // Carga de una matriz por teclado
-void cargarMatriz (vector<vector<int>>& matriz, int filas, int columnas) {
+void cargarMatriz (vector<vector<double>>& matriz, int filas, int columnas) {
 	for (int i = 0; i < filas; ++i) {
 		for (int j = 0; j < columnas; ++j) {
 			cout << "Ingrese el elemento [" << i << "][" << j << "]: ";
@@ -10,7 +10,7 @@ void cargarMatriz (vector<vector<int>>& matriz, int filas, int columnas) {
 	}
 }
 // Muestra la matriz formateada
-void mostrarMatriz (const vector<vector<int>>& matriz, int filas, int columnas) {
+void mostrarMatriz (const vector<vector<double>>& matriz, int filas, int columnas) {
 	for (int i = 0; i < filas; ++i) {
 		for (int j = 0; j < columnas; ++j) {
 			cout << matriz[i][j] << " ";
@@ -19,19 +19,19 @@ void mostrarMatriz (const vector<vector<int>>& matriz, int filas, int columnas) 
 	}
 }
 // Intercambia dos filas de la matriz
-void intercambiarFilas (vector<vector<int>>& matriz, int fila1, int fila2, int columnas) {
+void intercambiarFilas (vector<vector<double>>& matriz, int fila1, int fila2, int columnas) {
 	for (int j = 0; j < columnas; ++j) {
 		swap(matriz[fila1][j], matriz[fila2][j]);
 	}
 }
 // Multiplica una fila por un escalar
-void multiplicarFilaPorEscalar (vector<vector<int>>& matriz, int fila, int escalar, int columnas) {
+void multiplicarFilaPorEscalar (vector<vector<double>>& matriz, int fila, int escalar, int columnas) {
 	for (int j = 0; j < columnas; ++j) {
 		matriz[fila][j] *= escalar;
 	}
 }
 // Resta una fila origen a otra destino, multiplicando por un factor
-void restarFilas(std::vector<std::vector<int>>& mat, int filaPivote, int filaObjetivo, int factor, int columnas) {
+void restarFilas(std::vector<std::vector<double>>& mat, int filaPivote, int filaObjetivo, int factor, int columnas) {
     for (int j = 0; j < columnas; j++) {
         mat[filaObjetivo][j] -= factor * mat[filaPivote][j];
     }
@@ -77,6 +77,7 @@ double determinanteLaplace(const vector<vector<double>> &matriz) {
     }
     return det;
 }
+
 
 
 
