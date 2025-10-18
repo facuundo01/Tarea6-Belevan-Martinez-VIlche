cadb92b2ccd443332c0ad34d570387bccf16e3c0
Astinv <vilchecrece@gmail.com>
2025-10-17 22:27:23 -0300

    Update InDatos.cpp

--- InDatos.cpp
+++ InDatos.cpp
@@ -3,18 +3,6 @@
 #include <fstream>
 using namespace std;
 
-void ingresarSistema(vector<vector<double>> &matriz, vector<double> &terminos, int n) {
-    cout << "Ingrese los coeficientes de la matriz (" << n << "x" << n << "):\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << "a[" << i+1 << "][" << j+1 << "] = ";
-            cin >> matriz[i][j];
-        }
-        cout << "Termino independiente b[" << i+1 << "] = ";
-        cin >> terminos[i];
-    }
-}
-
 void guardarResultados(const vector<double> &soluciones, const string &archivo) {
     ofstream salida(archivo);
     if (!salida) {
@@ -28,4 +16,5 @@ void guardarResultados(const vector<double> &soluciones, const string &archivo) 
     salida.close();
     cout << "Resultados guardados en " << archivo << endl;
 }
+
 
