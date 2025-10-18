6b7e7c61b728927155b44c73cf14d2b84179d3f9
Astinv <vilchecrece@gmail.com>
2025-10-17 22:35:26 -0300

    Update Matrices.h

--- Matrices.h
+++ Matrices.h
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef MATRICES_H
+#define MATRICES_H
 
 #include <vector>
 #include <iostream>
@@ -8,4 +10,11 @@ void cargarMatriz(vector<vector<int>>& matriz, int filas, int columnas);
 void mostrarMatriz(const vector<vector<int>>& matriz, int filas, int columnas);
 void intercambiarFilas(vector<vector<int>>& matriz, int fila1, int fila2, int columnas);
 void multiplicarFilaPorEscalar(vector<vector<int>>& matriz, int fila, int escalar, int columnas);
+
 void restarFilas(std::vector<std::vector<int>>& mat, int filaPivote, int filaObjetivo, int factor, int columnas);
+
+double determinanteSarrus(const vector<vector<double>> &matriz);
+double determinanteLaplace(const vector<vector<double>> &matriz); // recursivo
+vector<vector<double>> menorMatriz(const vector<vector<double>> &matriz, int fila, int col);
+
+#endif
