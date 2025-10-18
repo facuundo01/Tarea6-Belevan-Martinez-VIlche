d8dfa87a6b3578e0a1327b3e6dce66ec02a24306
Astinv <vilchecrece@gmail.com>
2025-10-17 22:14:37 -0300

    Update InDatos.h

--- InDatos.h
+++ InDatos.h
@@ -1,1 +1,11 @@
-#pragma once
+#ifndef ENTRADA_SALIDA_H
+#define ENTRADA_SALIDA_H
+
+#include <vector>
+using namespace std;
+
+void ingresarSistema(vector<vector<double>> &matriz, vector<double> &terminos, int n);
+void mostrarMatriz(const vector<vector<double>> &matriz, const vector<double> &terminos);
+void guardarResultados(const vector<double> &soluciones, const string &archivo);
+
+#endif
